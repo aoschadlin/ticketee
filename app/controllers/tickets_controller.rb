db7217ecb4ea060a701d25a0b1292225d02746b3
Andre Oschadlin <andreoschadlin@gmail.com>
2011-06-07 11:18:03 -0700

    Implemented edit action for the tickets controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -17,6 +17,19 @@ class TicketsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @ticket.update_attributes(params[:ticket])
+      flash[:notice] = "Ticket has been updated."
+      redirect_to [@project, @ticket]
+    else
+      flash[:alert] = "Ticket has not been updated."
+      render :action => "edit"
+    end
+  end
+
   def find_ticket
     @ticket = @project.tickets.find(params[:id])
   end
